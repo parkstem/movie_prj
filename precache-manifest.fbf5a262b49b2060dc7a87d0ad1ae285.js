5926a4ddea2397f43339baa0f616a4a4af8d11b0
parkstem <qkrwnfrl@naver.com>
2019-06-24 20:00:10 +0900

    Updates

---
+++ precache-manifest.fbf5a262b49b2060dc7a87d0ad1ae285.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2313bf499d34975c8330d7289b4c2047",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "6fa39a9f76ac6779fba8",
+    "url": "/movie_app/static/css/main.8c5089d2.chunk.css"
+  },
+  {
+    "revision": "96e17dc3db81b7c313fa",
+    "url": "/movie_app/static/js/2.376568ed.chunk.js"
+  },
+  {
+    "revision": "6fa39a9f76ac6779fba8",
+    "url": "/movie_app/static/js/main.de9dcd2b.chunk.js"
+  },
+  {
+    "revision": "c6514544d436775abdc2",
+    "url": "/movie_app/static/js/runtime~main.175dda75.js"
+  }
+]);
